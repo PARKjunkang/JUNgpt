16aeaac772213cbdcf4f7fe66babf5018d131717
maverick <wnsrkd8753@gmail.com>
2024-08-08 17:07:11 +0900

    Add files via upload

---
+++ 로또/로또 2/a.js
@@ -0,0 +1,41 @@
+
+// 2. 내가 한 게임을 구매했다고 가정하고		
+
+// 컴퓨터의 6개의 숫자랑 내꺼랑 비교해서 몇개가 일치하는 지 세어 출력하기					 ex. 3개 일치	
+
+
+// 내 번호들
+var p1 = 12;
+var p2 = 13;
+
+var p3 = 22;
+var p4 = 30;
+var p5 = 41;
+var p6 = 45;
+
+// 랜덤 번호들
+var r1 = Math.floor(Math.random() * 45) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+document.write(r1);
+document.write("<br>");
+
+var r2 = Math.floor(Math.random() * 45) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+document.write(r2);
+document.write("<br>");
+
+var r3 = Math.floor(Math.random() * 45) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+document.write(r3);
+document.write("<br>");
+
+var r4 = Math.floor(Math.random() * 45) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+document.write(r4);
+document.write("<br>");
+
+var r5 = Math.floor(Math.random() * 45) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+document.write(r5);
+document.write("<br>");
+
+var r6 = Math.floor(Math.random() * 45) + 1; // 1 ~ n 까지 범위내에서 랜덤하게 숫자 하나 뽑아주는 애.
+document.write(r6);
+document.write("<br>");
+
+
